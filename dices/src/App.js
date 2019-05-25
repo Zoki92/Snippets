9baf5feee949de117edcbc7ced985db5974bc40a
Zoran Stoilov <zoranstoilov@yahoo.com>
2019-05-25 21:41:21 +0100

    added dice app

---
+++ dices/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Rolldice from "./Rolldice";
+
+function App() {
+  return (
+    <div className="App">
+      <Rolldice />
+    </div>
+  );
+}
+
+export default App;
